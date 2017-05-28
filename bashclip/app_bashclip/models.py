9f3d183ec470229cf0544b93b1480fcad4678e4c
josh <ashley.joshua@gmail.com>
2017-05-27 22:24:38 -0700

    stuff idk

--- bashclip/app_bashclip/models.py
+++ bashclip/app_bashclip/models.py
@@ -12,8 +12,15 @@ class Command(models.Model):
     line_no = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.base_cmd
+        return self.cmd_text
 
     def save(self, *args, **kwargs):
         # TODO: Parse base_cmd, flags, and args
+        base_cmd = self.cmd_text.split(' ')[0]
+        if 'sudo' in base_cmd:
+            self.base_cmd = 'sudo ' + base_cmd.split(' ')[1]
+        else:
+            self.base_cmd = base_cmd
+        # print(self.base_cmd, self.cmd_text)
+        print('i am sane')
         super(Command, self).save(*args, **kwargs)
